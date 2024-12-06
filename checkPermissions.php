4e5c83a3cb921705ab23c6d0c1f0a4c660b77345
johan.noy <johan.noy96@gmail.com>
2024-12-06 16:59:53 -0500

    check permissions

---
+++ checkPermissions.php
@@ -0,0 +1,39 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+error_reporting(E_ALL);
+header("Access-Control-Allow-Headers: Content-Type");
+ini_set('display_errors', 'On');
+
+try {
+    $ini = parse_ini_file('app.ini');
+    $servername = $ini['servername'];
+    $username = $ini['db_user'];
+    $password = $ini['db_password'];
+    $dbname = $ini['db_name'];
+
+    $name = $_GET['name'];
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM reservations WHERE  name = " . $name;
+    $stmt = $conn->prepare($sql);
+    $result = mysqli_query($conn, $sql);
+    $res = mysqli_fetch_assoc($result);
+    $permissions = $res['permissions'];
+
+    $conn->close();
+
+    var_dump($permissions);
+} catch (Exception $e) {
+    $response = array(
+        'message' => $e->getMessage()
+    );
+    echo json_encode($response);
+}
+die();
