b3307bd4e75b5352a083d9b2a535c9da972c0a31
johan.noy <johan.noy96@gmail.com>
2025-07-08 10:13:10 -0400

    send code added more text

--- sendCode.php
+++ sendCode.php
@@ -107,6 +107,9 @@ From the inside, turn the thumb to turn\n
 WiFi:\n
 User: ".$wifi_user."\n
 Password: ".$wifi_pass."\n
+Patients must access the apartment on the first day they were assigned before 10pm.\n
+If your door is locked or you cannot access it, please send a text message to the number: +1 (305) 713 5434 with the text: My door does not open.\n
+An agent will respond to you in the shortest time possible.\n
 ";
 
 $comment_text = "Apartment Reservation Note:\n
